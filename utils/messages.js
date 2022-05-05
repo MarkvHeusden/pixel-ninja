054e01e12af12eb5b719ec4c68e0fe25dd7476f7
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-05-05 16:30:46 +0200

    Refactoring

--- utils/messages.js
+++
@@ -1,9 +0,0 @@
-// import moment from 'moment'
-
-export default function formatMessage(username, content) {
-    return {
-        username,
-        content,
-        // time: moment().format('HH:mm'),
-    }
-}
