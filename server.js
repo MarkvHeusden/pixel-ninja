285143b10a3254670b4dbe57845f5291b2067ad7
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-05-06 23:43:56 +0200

    Track user positions in each rooms

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
-import { userJoin, getCurrentUser, userLeave, getRoomUsers } from './utils/users.js'
+import { gameInterval, createUser, getCurrentUser, userLeave, getRoomUsers } from './utils/users.js'
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+export const io = new Server(server)
 const port = process.env.PORT || 3002
 
 app.set('view engine', 'ejs')
@@ -29,17 +29,25 @@ app.post('/join-room', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('join-room', (username, roomId, x, y) => {
-        const user = userJoin(socket.id, username, roomId, x, y)
+        const user = createUser(socket.id, username, roomId, x, y)
+        const clientsInRoom = io.sockets.adapter.rooms.get(user.roomId)
+        const numOfClients = clientsInRoom ? clientsInRoom.size : 0
+
+        // If the room is new, create an game interval to track player positions
+        if (numOfClients < 1) {
+            gameInterval(roomId)
+        }
         socket.join(user.roomId)
         socket.emit('new-message', { username: botName, content: `Welkom ${user.username}` })
         socket.emit('show-character', user.character)
         socket.broadcast.to(user.roomId).emit('new-message', { username: botName, content: `${user.username} joined` })
         io.to(user.roomId).emit('current-users', getRoomUsers(user.roomId))
+        // console.log(io.sockets.adapter.rooms.get(user.roomId))
     })
 
-    socket.on('update-position', (data) => {
-        // console.log(`x: ${data.x}, y: ${data.y}`)
-        console.log(socket.id)
+    socket.on('player-moves', (direction) => {
+        const movingPlayer = getCurrentUser(socket.id)
+        movingPlayer.direction = direction
     })
 
     socket.on('send-message', (message) => {
